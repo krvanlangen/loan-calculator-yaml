1f20a75bdf9248aa2c282efe7aa2b2438888bf34
Kayla Van Langen <78875468+krvanlangen@users.noreply.github.com>
2023-04-17 20:52:41 -0500

    added 404 error page

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
 
 class Loan:
     def __init__(self, loanAmount, numberYears, annualRate):
